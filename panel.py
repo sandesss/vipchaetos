9ee7eb8dea995eaac6c59ea252bb18cc26d80970
sandess <niceguysande@gmail.com>
2026-09-13 22:52:06 +0800

    Enforce environment variable for firebase config

--- panel.py
+++ panel.py
@@ -7,15 +7,15 @@ from firebase_admin import credentials, db
 
 app = Flask(__name__)
 
-# Firebase Initialization using Environment Variable
+# Firebase Initialization strict environment variable check
 if not firebase_admin._apps:
     firebase_config_str = os.environ.get('FIREBASE_CONFIG_JSON')
-    if firebase_config_str:
-        cred_dict = json.loads(firebase_config_str)
-        cred = credentials.Certificate(cred_dict)
-    else:
-        cred = credentials.Certificate("firebase_credentials.json")
-        
+    if not firebase_config_str:
+        raise ValueError("CRITICAL: FIREBASE_CONFIG_JSON environment variable is missing!")
+    
+    cred_dict = json.loads(firebase_config_str)
+    cred = credentials.Certificate(cred_dict)
+    
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://vipchaetos-default-rtdb.firebaseio.com/'
     })
